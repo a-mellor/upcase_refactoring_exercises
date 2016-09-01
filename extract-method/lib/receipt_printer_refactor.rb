2e9509e314ebb9590038c28b3f5c3e233c4778fd
alex <alexander.mellor@gmail.com>
2016-09-01 15:34:42 +0100

    Extract-method. Refactored using Upcase solution

--- extract-method/lib/receipt_printer_refactor.rb
+++ extract-method/lib/receipt_printer_refactor.rb
@@ -4,7 +4,6 @@ class ReceiptPrinter
     'milk' => 3,
     'candy' => 1,
   }
-
   TAX = 0.05
 
   def initialize(output: $stdout, items:)
@@ -13,27 +12,57 @@ class ReceiptPrinter
   end
 
   def print
-    subtotal = items.reduce(0) do |sum, item|
-      item_cost = COST[item]
-      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
-
-      sum + item_cost.to_i
-    end
-
-    print_receipt_total(subtotal)
-  end
-
-  def print_receipt_total(subtotal)
-    output.puts divider
-    output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
-    output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
-    output.puts divider
-    output.puts "total: #{sprintf('$%.2f', subtotal + (subtotal * TAX))}"
+    output_items
+    output_divider
+    output_subtotals
+    output_divider
+    output_total
   end
 
   private
 
   attr_reader :output, :items
+
+  def output_items
+    items.each do |item|
+      output.puts "#{item}: #{sprintf('$%.2f', item_cost(item))}"
+    end
+  end
+
+  def item_cost(item)
+    COST[item]
+  end
+
+  def output_divider
+    output.puts divider
+  end
+
+  def output_subtotals
+    subtotal_line
+    tax_line
+  end
+
+  def subtotal_line
+    output_with(label: "subtotal", cost: subtotal)
+  end
+
+  def tax_line
+    output_with(label: "tax", cost: subtotal * TAX)
+  end
+
+  def subtotal
+    @_subtotal ||= items.reduce(0) do |sum, item|
+      sum +item_cost(item).to_i
+    end
+  end
+
+  def output_total
+    output_with(label: "total", cost: subtotal + (subtotal * TAX))
+  end
+
+  def output_with(label:, cost:)
+    output.puts "#{label}: #{sprintf('$%.2f', cost)}"
+  end
 
   def divider
     '-' * 13
